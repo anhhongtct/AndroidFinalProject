de3994bd39d0693a85a0d5b2a43841439c42a73b
anhhongtct <55115005+anhhongtct@users.noreply.github.com>
2019-11-19 20:02:51 -0500

    add more files into charge status

--- app/src/main/java/com/example/finalproject/LoadActivity.java
+++ app/src/main/java/com/example/finalproject/LoadActivity.java
@@ -1,4 +1,57 @@
 package com.example.finalproject;
 
-public class LoadActivity {
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class LoadActivity extends AppCompatActivity {
+
+    ArrayList<ChargingStation> chargingList = new ArrayList<>();
+    BaseAdapter myAdapter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.load_menu);
+
+        //List View
+        ListView theList =findViewById(R.id.theList);
+    }
+
+    public class MyListAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {return chargingList.size();
+        }
+
+        @Override
+        public ChargingStation getItem(int position) {
+            return chargingList.get(position);
+        }
+
+        @Override
+        public View getView(int position, View messView, ViewGroup parent) {
+            View thisRow = messView;
+                thisRow = getLayoutInflater().inflate(R.layout.list, null);
+                TextView sendText = thisRow.findViewById(R.id.viewText);
+                sendText.setText(String.valueOf(getItem(position).getLatitude()));
+            return thisRow;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return getItem(position).getId();
+        }
+    }
+
 }
--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,14 +1,86 @@
 package com.example.finalproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+    Toolbar tBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+
+        button1.setOnClickListener(clk -> startIntent(MainMenu.class));
+        tBar = findViewById(R.id.my_toolbar);
+        if (tBar != null) {
+            setSupportActionBar(tBar);//To display toolbar
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setElevation(0); // or other...
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_toolbar, menu);
+
+
+	    /* slide 15 material:
+	    MenuItem searchItem = menu.findItem(R.id.search_item);
+        SearchView sView = (SearchView)searchItem.getActionView();
+        sView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+            }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }  });
+
+	    */
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            //what to do when the menu item is selected:
+            case R.id.icon1:
+                break;
+            case R.id.icon2:
+                break;
+            case R.id.icon3:
+                break;
+            case R.id.icon4:
+                startIntent(MainMenu.class);
+                break;
+        }
+        return true;
+    }
+
+
+    private void startIntent(Class<?> cls) {
+        Intent intent = new Intent(this, cls);
+        startActivity(intent);
     }
 }
